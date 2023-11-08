28e7049074273b7b15e9ace1c1257a78e1a2e549
Markayel <mtorelus@gmail.com>
2023-11-07 19:34:14 -0800

    created a branch and files

---
+++ markayoport/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Welcome from "./Components/Welcome";
+import Projects from "./Components/Projects";
+import About from "./Components/About";
+import './App.css'
+
+const App = () => {
+return (
+<>
+<Welcome />
+<Projects />
+<About />
+</>
+
+)
+}
+
+
+export default App;
