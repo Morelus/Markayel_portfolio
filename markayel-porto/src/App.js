dd34b0855dade3c256af7923d7f3734bffe78ecf
Markayel <mtorelus@gmail.com>
2023-12-06 23:14:24 -0800

    added design to files

--- markayel-porto/src/App.js
+++ markayel-porto/src/App.js
@@ -1,32 +1,98 @@
-import React from "react";
-// import Welcome from "./Components/Welcome";
-// import Projects from "./Components/Projects";
-import About from "./Components/About";
-import Tom from "./Components/Tom";
-import './App.css'
-import myImage from "./Components/IMG_4270-removebg-preview.png"
-const App = () => {
-return (
-<>
-<div class= "welcome">
-      
-    </div>
-{/* <Projects /> */}
-<About />
-<Tom/>
-{/* <Welcome /> */}
-<div class= "picbox"> 
-<p> Female </p>
-<p> San Diego, CA</p>
-<p> United States</p>
-<p> Last Login:  </p>
-<img src={myImage}
-        alt="Paris Picture" 
-      /></div>
-</>
+import React from 'react';
+import './App.css';
 
-)
-}
+export const App= () => {
+    return (
+        <div className="index">
+          <div className="desktop">
+            <div className="overlap-group">
+              <div className="rectangle" />
+              <p className="text-wrapper">Apartment listing hkfhaksdf dkgjadjglaj hghadfgak kdahdkg.</p>
+              <p className="div">
+                Created the front and back-end components for a variation of tinder for cats, using ruby on rails and css.
+              </p>
+              <div className="text-wrapper-2">Morelus</div>
+              <img
+                className="img"
+                alt="Rectangle"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/rectangle-1.png"
+              />
+              <div className="rectangle-2" />
+              <img
+                className="linkedin"
+                alt="Linkedin"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/linkedin-1.png"
+              />
+              <img
+                className="github"
+                alt="Github"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/github-1.png"
+              />
+              <div className="rectangle-3" />
+              <img
+                className="logo"
+                alt="Logo"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/logo-1.png"
+              />
+              <div className="rectangle-4" />
+              <img
+                className="screenshot"
+                alt="Screenshot"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/screenshot-2023-12-01-at-3-58-1.png"
+              />
+              <div className="text-wrapper-3">Hi, I’m Markayel</div>
+              <div className="morelus" />
+              <div className="text-wrapper-4">Mtorelus@gmail.com</div>
+              <div className="morelus-2" />
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/line-1@2x.png"
+              />
+              <img
+                className="line-2"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/line-1@2x.png"
+              />
+              <img
+                className="line-3"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/line-3@2x.png"
+              />
+              <img
+                className="line-4"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/line-3@2x.png"
+              />
+              <div className="text-wrapper-5">Markayel Orelus.</div>
+              <div className="text-wrapper-6">Contact me.</div>
+              <div className="text-wrapper-7">Projects.</div>
+              <div className="text-wrapper-8">Home.</div>
+              <div className="text-wrapper-9">Projects</div>
+              <p className="p">Created an user interactive tic-tac-toe game, using React and css.</p>
+              <p className="text-wrapper-10">
+                Created the front and back-end components for a variation of tinder for cats, using ruby on rails and css.
+              </p>
+              <div className="rectangle-5" />
+              <p className="my-journey-from">
+                My journey from serving as a Hospital Corpsman in diverse managerial and medical care capacities to jumping
+                feet first into the ever exciting and growing world of software engineering has been a testament to my
+                adaptability and hunger for continuous growth. I am enthusiastic about the possibility of discussing how my
+                unique blend of skills and experiences align with the vision of a fun and evolving team. Thank you for
+                taking the time to visit my portfolio. <br />
+                Please contact me on LinkedIn at linkedin.com/morelus. I hope to hear from you soon.
+              </p>
+              <div className="text-wrapper-11">About me</div>
+              <p className="text-wrapper-12">REACT, RUBY ON RAILS, HTML, CSS</p>
+            </div>
+            <img
+              className="screenshot-2"
+              alt="Screenshot"
+              src="https://cdn.animaapp.com/projects/656fdef7436219044ee4dd06/releases/656ff6683bb4f21fb184bbd9/img/screenshot-2023-12-01-at-3-58-1.png"
+            />
+          </div>
+        </div>
+      );
+    };
 
-
-export default App;
+export default App
